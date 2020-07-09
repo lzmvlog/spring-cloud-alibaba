fc7618e50f36baf0c4ee551023d7517dbdc605da
ShaoJie <zhang1591313226@163.com>
2020-07-09 16:44:44 +0800

    整理 gradle 依赖

---
+++ nacos-user/src/main/java/com/shaojie/spring/cloud/alibaba/nacos/user/NacosUserApplication.java
@@ -0,0 +1,17 @@
+package com.shaojie.spring.cloud.alibaba.nacos.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ShaoJie
+ * @Date 2020年07月09 10:45
+ * @Description:
+ */
+@SpringBootApplication
+public class NacosUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosUserApplication.class);
+    }
+}
